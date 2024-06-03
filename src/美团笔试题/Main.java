c5f85e70aa499f95bede804cf29ed3d2f78d90f5
驰 <2497296400@qq.com>
2024-06-03 16:07:33 +0800

    提交

--- src/美团笔试题/Main.java
+++ src/美团笔试题/Main.java
@@ -1,74 +1,72 @@
-package 美团笔试题;
-
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
-    public static StreamTokenizer tokenizer = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));public static void main(String[] args) throws IOException {
-        int T = (int) nextInt();
-        for (int i = 0; i < T; i++) {
-            int m = (int) nextInt();
-            int[] arr = new int[m];
-            HashMap<Integer, Integer> map = new HashMap<>();
-            for (int j = 0; j < m; j++) {
-                arr[j] = nextInt();
-                map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
+    public static StreamTokenizer tokenizer = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int n = nextInt();
+        int m = nextInt();
+        int q = nextInt();
+        Set<Integer>[] graph = new Set[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new HashSet<>();
+        }
+        for (int i = 0; i < m; i++) {
+            int a = nextInt();
+            int b = nextInt();
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        int[] color = new int[n + 1];
+        int colorCount = 0;
+        for (int i = 1; i <= n; i++) {
+            if (color[i] == 0) {
+                colorCount++;
+                dfs(i, graph, color, colorCount);
             }
-            Arrays.sort(arr);
-            if (toCheck(arr, map)) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
+        }
+        for (int i = 0; i < q; i++) {
+            int op =nextInt();
+            int u = nextInt();
+            int v = nextInt();
+            if (op == 1) {
+                graph[u].remove(Integer.valueOf(v));
+                graph[v].remove(Integer.valueOf(u));
+                color = new int[n + 1];
+                colorCount = 0;
+                for (int j = 1; j <= n; j++) {
+                    if (color[j] == 0) {
+                        colorCount++;
+                        dfs(j, graph, color, colorCount);
+                    }
+                }
+            } else if (op == 2) {
+                if (color[u] == color[v]) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
             }
         }
     }
 
-    private static boolean toCheck(int[] arr, HashMap<Integer, Integer> map) {
-        long sum = 0L;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        if (sum % (arr.length / 2) != 0) {
-            return false;
-        }
-        int cur = (int) (sum / (arr.length / 2));
-        // System.out.println(cur);
-        for (int i = 0; i < arr.length; i++) {
-            if (map.isEmpty()) {
-                return true;
-            }
-            if (!map.containsKey(arr[i])) {
-                continue;
-            }
-            if (!map.containsKey(cur - arr[i])) {
-                return false;
-            } else {
-                Integer f = map.get(arr[i]);
-                Integer s = map.get(cur - arr[i]);
-                if (f == 1) {
-                    map.remove(arr[i]);
-                } else {
-                    map.put(arr[i], f - 1);
-                }
-                if (s == 1) {
-                    map.remove(cur - arr[i]);
-                } else {
-                    map.put(cur - arr[i], s - 1);
-
-                }
+    private static void dfs(int node, Set<Integer>[] graph, int[] color, int colorCount) {
+        color[node] = colorCount;
+        for (int neighbor : graph[node]) {
+            if (color[neighbor] == 0) {
+                dfs(neighbor, graph, color, colorCount);
             }
         }
-        return true;
     }
 
-    public static int nextInt() throws IOException {
-        tokenizer.nextToken();
+    public static int nextInt() {
+        try {
+            tokenizer.nextToken();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return (int) tokenizer.nval;
-    }
-
-    public static String nextString() throws IOException {
-        tokenizer.nextToken();
-        return tokenizer.sval;
     }
 }
